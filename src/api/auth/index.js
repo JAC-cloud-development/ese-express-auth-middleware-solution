9cdfb0fec4e0966a93a053884413bd13e7903024
mminuti <mminuti@runelab.it>
2022-02-14 16:46:46 +0100

    solution

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,12 +2,23 @@
 import _ from 'lodash'
 import { Router } from 'express';
 import { db } from '../../services/db/fakedb.js'
+import jwt from 'jsonwebtoken'
 
 const router = new Router();
 
 router.post("/login", function (request, response) {
-  console.log(request.headers)
-  response.sendStatus(200)
+  const decoded = Buffer.from(request.headers.authorization.split(" ")[1], 'base64').toString('binary')
+  const username = decoded.split(":")[0]
+  const user = _.find(db.users.list(), (u) => u.username = username)
+  if (user) {
+    const token = jwt.sign({
+      user: { id: user.id }
+    }, 'secret');
+    return response.json({
+      token
+    })
+  }
+  return response.sendStatus(401)
 });
 
 export default router;
